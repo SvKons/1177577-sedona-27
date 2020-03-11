3242d4965716fb70fac99bf6d46214a4ec0c93ba
SvKons <57892917+SvKons@users.noreply.github.com>
2020-03-11 21:44:50 +0300

    JS

--- js/popup.js
+++ js/popup.js
@@ -1,30 +1,14 @@
-var popup = document.querySelector(".form"); 
-   
-if (popup) {
-  popup.classList.toggle("form-show");
-}
 
 var button = document.querySelector(".modal-btn");
 
-var dateon = popup.querySelector("[name=dateon]");
-var dateoff = popup.querySelector("[name=dateoff]");
-var amount-adult = popup.querySelector("[name=amount-adult]");
-var amount-child = popup.querySelector("[name=amount-child]");
-var form = popup.querySelector("modal-form");
+var popup = document.querySelector(".form-show");
 
 button.addEventListener("click", function (evt) {
-    
-    evt.preventDefault();
-    popup.classList.toggle("form-show");
-    popup.classList.remove("modal-error");
-});
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  });
 
-form.addEventListener("submit", function (evt) {
-    if (!dateon.value || !dateoff.value || !amount-adult.value) {
-      evt.preventDefault();
-      popup.classList.remove("modal-error");
-      form.offsetWidth = form.offsetWidth;
-      popup.classList.add("modal-error")
-  }
-}
-);
+button.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-close");
+  });
